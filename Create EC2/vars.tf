8e39ef0874b91e81d5f9538ac40f40f590490f03
SAMUEL HAILE <samishken@gmail.com>
2019-10-14 17:47:31 -0400

    Update vars.tf
    AMI id updated

--- Create EC2/vars.tf
+++ Create EC2/vars.tf
@@ -13,8 +13,6 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
     us-east-2 = "ami-0d5d9d301c853a04a"
   }
 }
