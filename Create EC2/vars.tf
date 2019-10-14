962204f1eb8a552cad5e62febe1f253dbcf965e0
SAMUEL HAILE <samishken@gmail.com>
2019-10-14 17:45:11 -0400

    create ec2

---
+++ Create EC2/vars.tf
@@ -0,0 +1,21 @@
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    us-east-2 = "ami-0d5d9d301c853a04a"
+  }
+}
+
